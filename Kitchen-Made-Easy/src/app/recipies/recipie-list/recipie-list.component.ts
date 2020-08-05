441fd2f1b7961e635d6f608e59432d8c92a2e7ee
Soumyajit Dey <55289316+TheSoumyajitDey@users.noreply.github.com>
2020-08-05 09:58:42 +0530

    2 comes to an break circuit

--- Kitchen-Made-Easy/src/app/recipies/recipie-list/recipie-list.component.ts
+++ Kitchen-Made-Easy/src/app/recipies/recipie-list/recipie-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipie-list',
@@ -7,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipieListComponent implements OnInit {
 
+  recipies :Recipe[] = [
+    new Recipe("Test Recipe","just Testing","https://www.seriouseats.com/2020/05/20200504-vegetable-pancakes-sho-spaeth1.jpg"),
+    new Recipe("Test Recipe","just Testing","https://www.seriouseats.com/2020/05/20200504-vegetable-pancakes-sho-spaeth1.jpg")
+
+  ];
   constructor() { }
 
   ngOnInit(): void {
